ce3cf9e16a6fe59184d5e46826071d02b797ad26
pdpan0 <lm.m4rtins@gmail.com>
2023-11-07 21:33:28 -0300

    feat: configurando rotas

---
+++ lib/screens/landing.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LandingWidget extends StatefulWidget {
+  const LandingWidget({super.key});
+
+  @override
+  State<LandingWidget> createState() => _LandingWidgetState();
+}
+
+class _LandingWidgetState extends State<LandingWidget> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Landing Widget");
+  }
+}
