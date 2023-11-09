0dc8bcd8a4bc63cac61dcd6370d54ae350635a87
pdpan0 <lm.m4rtins@gmail.com>
2023-11-08 22:45:56 -0300

    feat: initial models

---
+++ lib/models/pet.dart
@@ -0,0 +1,9 @@
+import 'package:pet_health/models/race.dart';
+
+class Pet {
+  final int id;
+  final String name;
+  final Race race;
+
+  Pet(this.id, this.name, this.race);
+}
---
+++ lib/models/race.dart
@@ -0,0 +1,1 @@
+enum Race { cat, dog }
